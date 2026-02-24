8cdb7403d9344c7cdfecb99b24f7dae7e75acd09
Leonardo Pedrosa <leonardo.pedrosa@fcxlabs.com.br>
2026-02-23 21:29:16 -0300

    Adjusting for up in railway

--- FrontEnd/src/api/client.ts
+++ FrontEnd/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://financeproject.railway.internal/api',
+  baseURL: import.meta.env.VITE_API_URL ?? 'https://financeproject-backend-production.up.railway.app/api',
 })
 
 api.interceptors.request.use((config) => {
