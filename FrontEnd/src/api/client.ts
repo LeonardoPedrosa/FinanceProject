b14fb85d49aa601dc2a16e4289e24393f4d99952
Leonardo Pedrosa <leonardo.pedrosa@fcxlabs.com.br>
2026-02-23 21:18:41 -0300

    Removing file nginx

--- FrontEnd/src/api/client.ts
+++ FrontEnd/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? 'https://financeproject-backend-production.up.railway.app/api',
+  baseURL: 'https://financeproject.railway.internal/api',
 })
 
 api.interceptors.request.use((config) => {
